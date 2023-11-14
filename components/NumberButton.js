1e0fd40cf031e111c216b1793536c661e133db85
pravirchugh <pravirchugh@yahoo.com>
2023-11-14 02:18:12 -0800

    Reformatted Button files, commented out logging statements

--- components/NumberButton.js
+++ components/NumberButton.js
@@ -1,91 +1,92 @@
-import React, {useState} from 'react';
-import { Button, Text, TouchableOpacity, StyleSheet, View } from 'react-native';
+import React, { useState } from "react";
+import { Button, Text, TouchableOpacity, StyleSheet, View } from "react-native";
 
-const NumberButton = ({ value, isZero, firstArgument, setFirstArgument, secondArgument, setSecondArgument, displayValue, setDisplayValue, inProcess, setInProcess}) => {
-    const [count, setCount] = useState(0);
-    const onPress = () => {
-    
-        console.log(displayValue + " " + firstArgument + " " + secondArgument)
-        
-       if(!inProcess){
-    
-        
-        if(displayValue == "0" && firstArgument == "0"){
-            
+const NumberButton = ({
+  value,
+  isZero,
+  firstArgument,
+  setFirstArgument,
+  secondArgument,
+  setSecondArgument,
+  displayValue,
+  setDisplayValue,
+  inProcess,
+  setInProcess,
+}) => {
+  const [count, setCount] = useState(0);
+  const onPress = () => {
+    // console.log(displayValue + " " + firstArgument + " " + secondArgument)
 
-            console.log(1);
-            setFirstArgument(value.toString());
-            setDisplayValue(value.toString());
-
-            
-        } else {
-            console.log(2);
-            setFirstArgument(firstArgument + value.toString());
-            setDisplayValue(displayValue + value.toString());
-        }
-        
-
-        
-       } else {
-            if(secondArgument == "0"){
-                console.log(3)
-                setSecondArgument(value.toString());
-                setDisplayValue(value.toString());
-            } else {
-                console.log(4)
-                setSecondArgument(secondArgument + value.toString());
-                setDisplayValue(displayValue + value.toString());
-            }
-            
-       }
-
-        setCount(prevCount => prevCount + 1);
+    if (!inProcess) {
+      if (displayValue == "0" && firstArgument == "0") {
+        // console.log(1);
+        setFirstArgument(value.toString());
+        setDisplayValue(value.toString());
+      } else {
+        // console.log(2);
+        setFirstArgument(firstArgument + value.toString());
+        setDisplayValue(displayValue + value.toString());
+      }
+    } else {
+      if (secondArgument == "0") {
+        // console.log(3);
+        setSecondArgument(value.toString());
+        setDisplayValue(value.toString());
+      } else {
+        // console.log(4);
+        setSecondArgument(secondArgument + value.toString());
+        setDisplayValue(displayValue + value.toString());
+      }
     }
 
-    const inlineTextStyle = {textAlign: "center", color: "white", fontWeight: "bold", fontSize: 16 }
+    setCount((prevCount) => prevCount + 1);
+  };
 
-    return (
-       <>
-            {
-            
-                !isZero && <TouchableOpacity style={styles.button} onPress={onPress}>
-                    <Text style={inlineTextStyle}>{value}</Text>
-                </TouchableOpacity>
+  const inlineTextStyle = {
+    textAlign: "center",
+    color: "white",
+    fontWeight: "bold",
+    fontSize: 16,
+  };
 
-            }
+  return (
+    <>
+      {!isZero && (
+        <TouchableOpacity style={styles.button} onPress={onPress}>
+          <Text style={inlineTextStyle}>{value}</Text>
+        </TouchableOpacity>
+      )}
 
-            {
-                isZero && <TouchableOpacity style={styles.buttonZero} onPress={onPress}>
-                    <Text style={inlineTextStyle}>{value}</Text>
-                </TouchableOpacity>
-            }
-        </>
-    );
+      {isZero && (
+        <TouchableOpacity style={styles.buttonZero} onPress={onPress}>
+          <Text style={inlineTextStyle}>{value}</Text>
+        </TouchableOpacity>
+      )}
+    </>
+  );
 };
 
 const styles = StyleSheet.create({
-    
-    button: {
-        alignItems: 'center',
-        justifyContent: 'center',
-        backgroundColor: '#233C6E',
-        padding: 10,
-        margin: 2, 
-        // height: 100,
-        width: 100,
-        borderRadius: 20
-    },
-    buttonZero: {
-        alignItems: 'center',
-        justifyContent: 'center',
-        backgroundColor: '#233C6E',
-        padding: 10,
-        margin: 2, 
-        // height: 100,
-        width: 200,
-        borderRadius: 20
-    }
-    
+  button: {
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: "#233C6E",
+    padding: 10,
+    margin: 2,
+    // height: 100,
+    width: 100,
+    borderRadius: 20,
+  },
+  buttonZero: {
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: "#233C6E",
+    padding: 10,
+    margin: 2,
+    // height: 100,
+    width: 200,
+    borderRadius: 20,
+  },
 });
 
 export default NumberButton;
--- components/OperatorButton.js
+++ components/OperatorButton.js
@@ -1,158 +1,203 @@
-import React, {useState} from 'react';
-import { Button, Text, TouchableOpacity, StyleSheet } from 'react-native';
+import React, { useState } from "react";
+import { Button, Text, TouchableOpacity, StyleSheet } from "react-native";
 
-const OperatorButton = ({ text, firstArgument, setFirstArgument, secondArgument, setSecondArgument, displayValue, setDisplayValue, inProcess, setInProcess, operator, setOperator }) => {
-    const [count, setCount] = useState(0);
-    const onPress = () => {
-        if(text == "C"){
-            setDisplayValue(0);
-            if(!inProcess){
-                setFirstArgument(0);
-            } else {
-                setSecondArgument(0);
-            }
-        } else if(text == "⌫"){
-
-            if(displayValue.length == 1){
-                setDisplayValue(0);
-                if(!inProcess){
-                    setFirstArgument(0);
-                } else {
-                    setSecondArgument(0);
-                }
-            } else if(displayValue != 0){
-                setDisplayValue(displayValue.substring(0,  displayValue.length - 1))
-                if(!inProcess){
-                    setFirstArgument(0);
-                } else {
-                    setSecondArgument(0);
-                }
-            } else if(displayValue == 0){
-                setDisplayValue(0);
-            }
-        } else if(text == "+/-"){
-            setDisplayValue((-1 * Number(displayValue)).toString());
-            if(!inProcess){
-                setFirstArgument(-1 * firstArgument);
-            } else {
-                setSecondArgument(-1 * secondArgument);
-            }
-
-        } else if(text == "."){
-            setDisplayValue(displayValue + ".");
-        } else if(text == "%"){
-            setDisplayValue((0.01 * Number(displayValue)).toString());
-            if(!inProcess){
-                setFirstArgument(0.01 * firstArgument);
-            } else {
-                setSecondArgument(0.01 * secondArgument);
-            }
-
-        } else if(text == "1/x"){
-            setDisplayValue((1 / Number(displayValue)).toString());
-            if(!inProcess){
-                setFirstArgument(1 / firstArgument);
-            } else {
-                setSecondArgument(1 / secondArgument);
-            }
-        } else if(text == "x²"){
-
-            let temporaryValue = Number(displayValue) * Number(displayValue);
-
-            if(temporaryValue <= Math.floor(temporaryValue) + 0.0000001){ // this accounts for a precision error 
-                temporaryValue = Math.floor(temporaryValue);
-            }
-            if(temporaryValue >= Math.ceil(temporaryValue) - 0.0000001){ // this accounts for a precision error 
-                temporaryValue = Math.ceil(temporaryValue);
-            }
-
-            setDisplayValue((temporaryValue).toString());
-            if(!inProcess){
-                setFirstArgument(temporaryValue);
-            } else {
-                setSecondArgument(temporaryValue);
-            }
-        } else if(text == "√"){
-
-            setDisplayValue((Math.sqrt(Number(displayValue))).toString());
-            if(!inProcess){
-                setFirstArgument(firstArgument * firstArgument);
-            } else {
-                setSecondArgument(secondArgument * secondArgument);
-            }
+const OperatorButton = ({
+  text,
+  firstArgument,
+  setFirstArgument,
+  secondArgument,
+  setSecondArgument,
+  displayValue,
+  setDisplayValue,
+  inProcess,
+  setInProcess,
+  operator,
+  setOperator,
+}) => {
+  const [count, setCount] = useState(0);
+  const onPress = () => {
+    if (text == "C") {
+      setDisplayValue(0);
+      if (!inProcess) {
+        setFirstArgument(0);
+      } else {
+        setSecondArgument(0);
+      }
+    } else if (text == "⌫") {
+      if (displayValue.length == 1) {
+        setDisplayValue(0);
+        if (!inProcess) {
+          setFirstArgument(0);
+        } else {
+          setSecondArgument(0);
         }
-        
-        else {
-            // Pressed one of the four following operators: + - * / OR =
-            
-            if(text == "="){
-                // EVALUATE
-                setInProcess(false);
-                if(operator == '+' || operator == "-" || operator == "*" || operator =="/"){
-                    switch (operator) {
-                      case "+":
-                        setDisplayValue((parseFloat(firstArgument) + parseFloat(secondArgument)).toString());
-                        setFirstArgument((parseFloat(firstArgument) + parseFloat(secondArgument)).toString());
-                        setSecondArgument(0);
-                        break;
-              
-                      case "-":
-                        setDisplayValue((parseFloat(firstArgument) - parseFloat(secondArgument)).toString());
-                        setFirstArgument((parseFloat(firstArgument) - parseFloat(secondArgument)).toString());
-                        setSecondArgument(0);
-                        break;
-              
-                      case "*":
-                        setDisplayValue((parseFloat(firstArgument) * parseFloat(secondArgument)).toString());
-                        setFirstArgument((parseFloat(firstArgument) * parseFloat(secondArgument)).toString());
-                        setSecondArgument(0);
-                        break;
-              
-                      case "/":
-                        setDisplayValue((parseFloat(firstArgument) / parseFloat(secondArgument)).toString());
-                        setFirstArgument((parseFloat(firstArgument) / parseFloat(secondArgument)).toString());
-                        setSecondArgument(0);
-                        break;
-                    
-                      default:
-                        break;
-                    }
-                  }
-            } else {
-                // text == one of the four operators
-                setOperator(text);
-                setInProcess(true);
-        
-                setSecondArgument(0);
-                setFirstArgument(displayValue);
-            }
+      } else if (displayValue != 0) {
+        setDisplayValue(displayValue.substring(0, displayValue.length - 1));
+        if (!inProcess) {
+          setFirstArgument(0);
+        } else {
+          setSecondArgument(0);
         }
+      } else if (displayValue == 0) {
+        setDisplayValue(0);
+      }
+    } else if (text == "+/-") {
+      setDisplayValue((-1 * Number(displayValue)).toString());
+      if (!inProcess) {
+        setFirstArgument(-1 * firstArgument);
+      } else {
+        setSecondArgument(-1 * secondArgument);
+      }
+    } else if (text == ".") {
+      setDisplayValue(displayValue + ".");
+    } else if (text == "%") {
+      setDisplayValue((0.01 * Number(displayValue)).toString());
+      if (!inProcess) {
+        setFirstArgument(0.01 * firstArgument);
+      } else {
+        setSecondArgument(0.01 * secondArgument);
+      }
+    } else if (text == "1/x") {
+      setDisplayValue((1 / Number(displayValue)).toString());
+      if (!inProcess) {
+        setFirstArgument(1 / firstArgument);
+      } else {
+        setSecondArgument(1 / secondArgument);
+      }
+    } else if (text == "x²") {
+      let temporaryValue = Number(displayValue) * Number(displayValue);
+
+      if (temporaryValue <= Math.floor(temporaryValue) + 0.0000001) {
+        // this accounts for a precision error
+        temporaryValue = Math.floor(temporaryValue);
+      }
+      if (temporaryValue >= Math.ceil(temporaryValue) - 0.0000001) {
+        // this accounts for a precision error
+        temporaryValue = Math.ceil(temporaryValue);
+      }
+
+      setDisplayValue(temporaryValue.toString());
+      if (!inProcess) {
+        setFirstArgument(temporaryValue);
+      } else {
+        setSecondArgument(temporaryValue);
+      }
+    } else if (text == "√") {
+      setDisplayValue(Math.sqrt(Number(displayValue)).toString());
+      if (!inProcess) {
+        setFirstArgument(firstArgument * firstArgument);
+      } else {
+        setSecondArgument(secondArgument * secondArgument);
+      }
+    } else {
+      // Pressed one of the four following operators: + - * / OR =
+
+      if (text == "=") {
+        // EVALUATE
+        setInProcess(false);
+        if (
+          operator == "+" ||
+          operator == "-" ||
+          operator == "*" ||
+          operator == "/"
+        ) {
+          switch (operator) {
+            case "+":
+              setDisplayValue(
+                (
+                  parseFloat(firstArgument) + parseFloat(secondArgument)
+                ).toString()
+              );
+              setFirstArgument(
+                (
+                  parseFloat(firstArgument) + parseFloat(secondArgument)
+                ).toString()
+              );
+              setSecondArgument(0);
+              break;
+
+            case "-":
+              setDisplayValue(
+                (
+                  parseFloat(firstArgument) - parseFloat(secondArgument)
+                ).toString()
+              );
+              setFirstArgument(
+                (
+                  parseFloat(firstArgument) - parseFloat(secondArgument)
+                ).toString()
+              );
+              setSecondArgument(0);
+              break;
+
+            case "*":
+              setDisplayValue(
+                (
+                  parseFloat(firstArgument) * parseFloat(secondArgument)
+                ).toString()
+              );
+              setFirstArgument(
+                (
+                  parseFloat(firstArgument) * parseFloat(secondArgument)
+                ).toString()
+              );
+              setSecondArgument(0);
+              break;
+
+            case "/":
+              setDisplayValue(
+                (
+                  parseFloat(firstArgument) / parseFloat(secondArgument)
+                ).toString()
+              );
+              setFirstArgument(
+                (
+                  parseFloat(firstArgument) / parseFloat(secondArgument)
+                ).toString()
+              );
+              setSecondArgument(0);
+              break;
+
+            default:
+              break;
+          }
+        }
+      } else {
+        // text == one of the four operators
+        setOperator(text);
+        setInProcess(true);
+
+        setSecondArgument(0);
+        setFirstArgument(displayValue);
+      }
     }
+  };
 
-    
+  const inlineTextStyle = {
+    textAlign: "center",
+    color: "white",
+    fontWeight: "bold",
+    fontSize: 16,
+  };
 
-    const inlineTextStyle = {textAlign: "center", color: "white", fontWeight: "bold", fontSize: 16 }
-
-    return (
-        <TouchableOpacity style={styles.button} onPress={onPress}>
-            <Text style={inlineTextStyle}>{text}</Text>
-        </TouchableOpacity>
-    );
+  return (
+    <TouchableOpacity style={styles.button} onPress={onPress}>
+      <Text style={inlineTextStyle}>{text}</Text>
+    </TouchableOpacity>
+  );
 };
 
 const styles = StyleSheet.create({
-    
-    button: {
-        alignItems: 'center',
-        justifyContent: 'center',
-        backgroundColor: '#233C6E',
-        padding: 10,
-        margin: 2, 
-        // height: 100,
-        width: 100,
-        borderRadius: 20
-    },
-    
+  button: {
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: "#233C6E",
+    padding: 10,
+    margin: 2,
+    // height: 100,
+    width: 100,
+    borderRadius: 20,
+  },
 });
 
 export default OperatorButton;
